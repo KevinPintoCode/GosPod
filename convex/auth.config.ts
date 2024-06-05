935bf6d7a1f019c15df8d97a470c43ffca416f77
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-06-05 15:10:30 +0200

    setting up Clerk Auth with Convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "//https://sunny-bear-6.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
