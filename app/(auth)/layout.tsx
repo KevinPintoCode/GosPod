8951b0d832271b2759c5db8e53ddf412b8561cb0
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-06-06 15:11:00 +0200

    styles of auth pages and finish clerk setup.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,17 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <main>{children}</main>
+      <main className="relative h-scree w-full">
+        <div className="absolute size-full">
+          <Image
+            src="/images/bg-img.png"
+            alt="Background"
+            fill
+            className="size-full"
+          />
+        </div>
+        {children}
+      </main>
     </>
   );
 }
---
+++ app/(auth)/sing-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { SignUp } from "@clerk/clerk-react";
+
+const Page = () => {
+  return (
+    <div className="flex-center glassmorphism-auth h-screen w-full">
+      <SignUp />
+    </div>
+  );
+};
+
+export default Page;
