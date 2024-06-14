35ed5cf9f388710416458ebd4e714961f162870f
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-06-14 23:18:57 +0200

    Start developing the Generating thumbnail component

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 const GenerateThumbnail = () => {
-  return <div>GenerateThumbnail</div>;
+  const [isAiThumbnail, setisAiThumbnail] = useState(false);
+  return (
+    <>
+      <div className="generate_thumbnail">
+        <Button
+          type="button"
+          variant="plain"
+          className={cn("", { "bg-black-6": isAiThumbnail })}
+        >
+          Use AI to generate a thumbnail.
+        </Button>
+        <Button
+          type="button"
+          variant="plain"
+          className={cn("", { "bg-black-6": !isAiThumbnail })}
+        >
+          Upload your thumbnail.
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default GenerateThumbnail;
