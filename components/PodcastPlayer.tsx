1234c260d1d35c3bf7def6701d00dd3f856fd44a
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-15 13:20:24 +0200

    added progress bar and fetch data from the context

--- components/PodcastPlayer.tsx
+++ components/PodcastPlayer.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useAudio } from "@/providers/AudioProvider";
+import React, { useState } from "react";
+
+import { Progress } from "@/components/ui/progress";
+import Image from "next/image";
 
 const PodcastPlayer = () => {
-  return <div>PodcastPlayer</div>;
+  const { audio } = useAudio();
+
+  const [currentTime, setCurrentTime] = useState(10);
+  const [duration, setDuration] = useState(100);
+
+  return (
+    <div
+      className={cn("sticky bottom-0 left-0 flex size-full flex-col", {
+        hidden: !audio?.audioUrl,
+      })}
+    >
+      <h1 className="text-white-1 text-xl">{audio?.title}</h1>
+      <Progress
+        className="w-full"
+        max={duration}
+        value={(currentTime / duration) * 100}
+      />
+      <Image
+        src={audio?.imageUrl!}
+        alt="player"
+        height={64}
+        width={64}
+        className="aspect-square rounded-xl"
+      />
+      <h1 className="text-white-1 text-xl">{audio?.author}</h1>
+    </div>
+  );
 };
 
 export default PodcastPlayer;
