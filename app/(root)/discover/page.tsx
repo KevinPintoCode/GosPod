bb37a1d94b75048ea0420f3eca37de6b73966732
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-16 23:42:34 +0200

    logic for query of discover page

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import EmptyState from "@/components/EmptyState";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 import React from "react";
 
 const Discover = () => {
+  const podcastData = useQuery(api.podcasts.getPodcastBySearch, { search: "" });
+
   return (
     <>
-      <div>
-        <h1 className="text-20 font-bold text-white-1">Discover</h1>
+      <div className="flex flex-col gap-9 ">
+        Searchbar
+        <div className="flex flex-col gap-9">
+          <h1 className="text-20 font-bold text-white-1">Discover</h1>
+          {podcastData ? <></> : <EmptyState title="No results found" />}
+        </div>
       </div>
     </>
   );
