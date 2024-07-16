40a631e0c9d242805380c1dace1cac7a028e1eba
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-16 23:59:02 +0200

    added Search bar and hook to update the query

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -3,6 +3,7 @@
 import EmptyState from "@/components/EmptyState";
 import LoaderSpinner from "@/components/LoaderSpinner";
 import PodcastCard from "@/components/PodcastCard";
+import Searchbar from "@/components/Searchbar";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import React from "react";
@@ -15,7 +16,7 @@ const Discover = () => {
   return (
     <>
       <div className="flex flex-col gap-9 ">
-        Searchbar
+        <Searchbar />
         <div className="flex flex-col gap-9">
           <h1 className="text-20 font-bold text-white-1">Discover</h1>
           {podcastsData ? (
