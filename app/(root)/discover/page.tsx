33ffd4ef0d9558a27732986cd8fdc5d76e857535
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-16 23:50:22 +0200

    added podcast grid to discover

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import EmptyState from "@/components/EmptyState";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import PodcastCard from "@/components/PodcastCard";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import React from "react";
 
 const Discover = () => {
-  const podcastData = useQuery(api.podcasts.getPodcastBySearch, { search: "" });
+  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, {
+    search: "",
+  });
 
   return (
     <>
@@ -14,7 +18,29 @@ const Discover = () => {
         Searchbar
         <div className="flex flex-col gap-9">
           <h1 className="text-20 font-bold text-white-1">Discover</h1>
-          {podcastData ? <></> : <EmptyState title="No results found" />}
+          {podcastsData ? (
+            <>
+              {podcastsData.length > 0 ? (
+                <div className="podcast_grid">
+                  {podcastsData?.map(
+                    ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                      <PodcastCard
+                        key={_id}
+                        imgUrl={imageUrl!}
+                        title={podcastTitle}
+                        description={podcastDescription}
+                        podcastId={_id}
+                      />
+                    )
+                  )}
+                </div>
+              ) : (
+                <EmptyState title="No results found" />
+              )}
+            </>
+          ) : (
+            <LoaderSpinner />
+          )}
         </div>
       </div>
     </>
