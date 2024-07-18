bd902032fd7ee3ccd641f114e3a44fe8bd2b9dd6
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-18 23:46:04 +0200

    Start building mobilenavbar with shadcn sheet component

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,50 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+
 import React from "react";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <section>
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="navbar"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-black-1">
+          <Link
+            href="/"
+            className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+          >
+            <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+            <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+              GosPod
+            </h1>
+          </Link>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
 };
 
 export default MobileNav;
