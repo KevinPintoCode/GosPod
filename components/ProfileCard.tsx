ec296c0636900a8b18d16543da85fef52b629e0d
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-22 23:56:47 +0200

    ProfileCard component fix

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-//import { useAudio } from "@/providers/AudioProvider";
+import { useAudio } from "@/providers/AudioProvider";
 import { PodcastProps, ProfileCardProps } from "@/types";
 
 import LoaderSpinner from "./LoaderSpinner";
