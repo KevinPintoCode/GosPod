82d2bce2f0bdcec57fdc50360ef554f5159190eb
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-22 23:54:46 +0200

    added .map to all podcast for the user

--- app/(root)/profile/[profileId]/page.tsx
+++ app/(root)/profile/[profileId]/page.tsx
@@ -36,7 +36,27 @@ const ProfilePage = ({
       </div>
       <section className="mt-9 flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">All Podcasts</h1>
-        Podcast data
+        {podcastsData && podcastsData.podcasts.length > 0 ? (
+          <div className="podcast_grid">
+            {podcastsData?.podcasts
+              ?.slice(0, 4)
+              .map((podcast) => (
+                <PodcastCard
+                  key={podcast._id}
+                  imgUrl={podcast.imageUrl!}
+                  title={podcast.podcastTitle!}
+                  description={podcast.podcastDescription}
+                  podcastId={podcast._id}
+                />
+              ))}
+          </div>
+        ) : (
+          <EmptyState
+            title="You have not created any podcasts yet"
+            buttonLink="/create-podcast"
+            buttonText="Create Podcast"
+          />
+        )}
       </section>
     </section>
   );
