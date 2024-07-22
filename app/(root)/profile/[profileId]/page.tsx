b7420f6c5a430e135a79b36668e24cd3b55fb6e5
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-22 23:51:23 +0200

    bug while showing profileId page

---
+++ app/(root)/profile/[profileId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = ({
+  params: { profileId },
+}: {
+  params: { profileId: string };
+}) => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
