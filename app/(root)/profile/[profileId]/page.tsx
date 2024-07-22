56a8ede5d9f4d05ed284881f5595b57790e4870a
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-07-22 23:54:28 +0200

    base profileId layout

--- app/(root)/profile/[profileId]/page.tsx
+++ app/(root)/profile/[profileId]/page.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+"use client";
 
-const Profile = ({
-  params: { profileId },
+import { useQuery } from "convex/react";
+
+import EmptyState from "@/components/EmptyState";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import PodcastCard from "@/components/PodcastCard";
+import ProfileCard from "@/components/ProfileCard";
+import { api } from "@/convex/_generated/api";
+
+const ProfilePage = ({
+  params,
 }: {
-  params: { profileId: string };
+  params: {
+    profileId: string;
+  };
 }) => {
-  return <div>Profile</div>;
+  const user = useQuery(api.users.getUserById, {
+    clerkId: params.profileId,
+  });
+  const podcastsData = useQuery(api.podcasts.getPodcastByAuthorId, {
+    authorId: params.profileId,
+  });
+
+  return (
+    <section className="mt-9 flex flex-col">
+      <h1 className="text-20 font-bold text-white-1 max-md:text-center">
+        GosPod Profile
+      </h1>
+      <div className="mt-6 flex flex-col gap-6 max-md:items-center md:flex-row">
+        <ProfileCard
+          podcastData={podcastsData!}
+          imageUrl={user?.imageUrl!}
+          userFirstName={user?.name!}
+        />
+      </div>
+      <section className="mt-9 flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">All Podcasts</h1>
+        Podcast data
+      </section>
+    </section>
+  );
 };
 
-export default Profile;
+export default ProfilePage;
